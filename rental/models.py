fd4c0edb7efb5471858acbb016266f166516d04d
Hezatamalegesh <116362855+Hezatamalegesh@users.noreply.github.com>
2024-06-04 22:46:40 +0200

    2024-06-04 DM

--- rental/models.py
+++ rental/models.py
@@ -4,77 +4,116 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Equipment(models.Model):
-    equipment = models.CharField(max_length=50)
-    pass
+    equipment = models.CharField(max_length=50, verbose_name=_('Equipment'))
+
+    def __str__(self) -> str:
+        return self.equipment
+
+    class Meta:
+        verbose_name = _("Equipment")
+        verbose_name_plural = _("Equipments")
 
 class Car(models.Model):
     ENGINE_TYPES = [
-        ("benzynowy", "Benzynowy"),
-        ("diesel", "Diesel"),
-        ("hybrydowy", "Hybrydowy"),
-        ("elektryczny", "Elektryczny"),
-        ("wodorowy", "Wodorowy")
+        ("petrol", _("Petrol")),
+        ("diesel", _("Diesel")),
+        ("hybrid", _("Hybrid")),
+        ("electric", _("Electric")),
+        ("hydrogen", _("Hydrogen"))
     ]
     GEARBOX_TYPES = [
-        ("automatyczna", "Automatyczna"),
-        ("manualna", "Manualna"),
-        ("pol_automatyczna", "Polautomatyczna"),
+        ("automatic", _("Automatic")),
+        ("manual", _("Manual")),
+        ("semi-automatic", _("Semi-automatic")),
     ]
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    engine_type = models.CharField(max_length=20, choices=ENGINE_TYPES)
-    seats_count = models.PositiveBigIntegerField()
-    doors_count = models.PositiveSmallIntegerField()
-    fuel_usage = models.FloatField()
-    engine_power = models.PositiveSmallIntegerField()
-    color = models.CharField(max_length=20)
-    equipment = models.ManyToManyField(Equipment)
-    gearbox_types = models.CharField(max_length=20, choices=GEARBOX_TYPES)
-    available = models.BooleanField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    value = models.DecimalField(max_digits=10, decimal_places=2)
-    pass
+    CATEGORIES = [
+        ("suv", _("SUV")),
+        ("city-friendly", _("City-friendly")),
+        ("off-road", _("Off-road")),
+        ("van", _("Van")),
+        ("sports", _("Sports")),
+    ]
+    category = models.CharField(max_length=50, choices=CATEGORIES, verbose_name=_('Category'))
+    brand = models.CharField(max_length=50, verbose_name=_('Brand'))
+    model = models.CharField(max_length=50, verbose_name=_('Model'))
+    engine_type = models.CharField(max_length=20, choices=ENGINE_TYPES, verbose_name=_('Engine type'))
+    seats_count = models.PositiveBigIntegerField(verbose_name=_('Number of seats'))
+    doors_count = models.PositiveSmallIntegerField(verbose_name=_('Number of doors'))
+    fuel_usage = models.FloatField(verbose_name=_('Fuel consumption'))
+    engine_power = models.PositiveSmallIntegerField(verbose_name=_('Engine power'))
+    color = models.CharField(max_length=20, verbose_name=_('Color'))
+    equipment = models.ManyToManyField(Equipment, verbose_name=_('Equipment'))
+    gearbox_type = models.CharField(max_length=20, choices=GEARBOX_TYPES, verbose_name=_('Gearbox type'))
+    available = models.BooleanField(verbose_name=_('Availability'))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Price'))
+    value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Value'))
 
+    def __str__(self) -> str:
+        return f"{self.brand} {self.model} (ID: {self.id})"
 
+    class Meta:
+        verbose_name = _("Car")
+        verbose_name_plural = _("Cars")
+
+class UserData(User):
+    IDENTITY_DOCUMENT_TYPES = [
+        ("identity_card", _("Identity card")),
+        ("passport", _("Passport")),
+        ("driver_license", _("Driver's license")),
+    ]
+    phone = models.CharField(max_length=20, verbose_name=_('Phone number'))
+    identity_document_no = models.CharField(max_length=20, verbose_name=_('Identity document number'))
+    identity_document_type = models.CharField(max_length=20, choices=IDENTITY_DOCUMENT_TYPES, verbose_name=_('Identity document type'))
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+    class Meta:
+        verbose_name = _("User data")
+        verbose_name_plural = _("User data (Rental)")
+        app_label = 'auth'
 
 class Address(models.Model):
-    user = models.OneToOneField(User, on_delete=models.RESTRICT)
-    country = models.CharField(max_length=30)
-    city = models.CharField(max_length=30)
-    post_code = models.CharField(max_length=10)
-    street = models.CharField(max_length=50)
-    building_no = models.CharField(max_length=10)
-    appartment_no = models.CharField(max_length=10, blank=True, default='', help_text='To pole jest opcjonalne.')
-    pass
+    user = models.OneToOneField(UserData, on_delete=models.CASCADE, verbose_name=_('User'))
+    country = models.CharField(max_length=30, verbose_name=_('Country'))
+    city = models.CharField(max_length=30, verbose_name=_('City'))
+    post_code = models.CharField(max_length=10, verbose_name=_('Post code'))
+    street = models.CharField(max_length=50, verbose_name=_('Street'))
+    building_no = models.CharField(max_length=10, verbose_name=_('Building number'))
+    appartment_no = models.CharField(max_length=10, blank=True, default='', help_text=_('This field is optional.'), verbose_name=_('Apartment number'))
 
-class User(User):
-    IDENTITY_DOCUMENT_TYPES = [
-        ("dowod", "Dowód osobisty"),
-        ("passport", "Paszport"),
-        ("prawo_jazdy", "Prawo jazdy"),
-    ]
-    phone = models.CharField(max_length=20)
-    identity_document_no = models.CharField(max_length=20)
-    identity_document_type = models.CharField(max_length=20, choices=IDENTITY_DOCUMENT_TYPES)
-    pass
+    def __str__(self) -> str:
+        if self.appartment_no:
+            return f"{self.street} {self.building_no} / {self.appartment_no}, {self.post_code} {self.city}, {self.country}"
+        else:
+            return f"{self.street} {self.building_no}, {self.post_code} {self.city}, {self.country}"
 
+    class Meta:
+        verbose_name = _("Address")
+        verbose_name_plural = _("Addresses")
 
 class Order(models.Model):
     PAYMENT_METHODS = [
-        ("karta", "Karta"),
-        ("gotowka", "Gotówka"),
-        ("przelew", "Przelew"),
-        ("blik", "Blik"),
+        ("card", _("Card")),
+        ("cash", _("Cash")),
+        ("transfer", _("Transfer")),
+        ("blik", _("Blik")),
     ]
-    customer = models.ForeignKey(User, on_delete=models.RESTRICT)
-    car = models.ForeignKey(Car, on_delete=models.RESTRICT)
-    order_value = models.DecimalField(max_digits=10, decimal_places=2)
-    declared_order_duration = models.DurationField()
-    pickup_date = models.DateTimeField()
-    return_date = models.DateTimeField()
-    deposit = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
-    payment_status = models.BooleanField()
-    pass
+    customer = models.ForeignKey(UserData, on_delete=models.RESTRICT, verbose_name=_('Customer'))
+    car = models.ForeignKey(Car, on_delete=models.RESTRICT, verbose_name=_('Car'))
+    order_value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Order value'))
+    declared_order_duration = models.DurationField(verbose_name=_('Declared rental duration'))
+    pickup_date = models.DateTimeField(verbose_name=_('Pickup date'))
+    return_date = models.DateTimeField(verbose_name=_('Return date'))
+    deposit = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Deposit'))
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS, verbose_name=_('Payment method'))
+    payment_status = models.BooleanField(verbose_name=_('Payment status'))
 
+    def __str__(self) -> str:
+        return f"Order ID: {self.id} ({_('Paid') if self.payment_status else _('Unpaid')})"
+
+    class Meta:
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
--- rental/views.py
+++ rental/views.py
@@ -4,8 +4,22 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html.jinja')
 
-def index(request):
-    return render(request, 'cars.html.jinja')
+def cars(request):
+    cars = Car.objects.all()
+    cars_table = pd.DataFrame.from_records(cars.values())
+
+    # Słownik mapujący kategorie na ich przyjazne użytkownikowi nazwy
+    category_dict = dict(Car.CATEGORIES)
+
+    # Tworzenie struktury danych dla szablonu
+    cars_by_category = {}
+    for car in cars:
+        category_name = category_dict[car.category]
+        if category_name not in cars_by_category:
+            cars_by_category[category_name] = []
+        cars_by_category[category_name].append(car)
+
+    return render(request, 'cars.html.jinja', {'cars_by_category': cars_by_category, 'cars_table': cars_table.to_html(classes='table table-striped table-bordereds', index=False, table_id='cars_table')}, )
 
 def index(request, car_id):
     return render(request, 'car.html.jinja')
